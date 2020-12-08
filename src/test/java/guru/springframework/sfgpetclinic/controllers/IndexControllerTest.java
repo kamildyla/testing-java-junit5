f675ee819d45362a9d28f8f5e7d1a887232b15a6
kamildyla <kamil.dyla09@gmail.com>
2020-12-08 20:49:25 +0100

    test Timeout; test assuptions

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -27,5 +31,33 @@ class IndexControllerTest {
         assertThrows(ValueNotFoundException.class, () -> {
             controller.oopsHandler();
         });
+    }
+
+    @Disabled
+    @Test
+    void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(1000);
+            System.out.println("I got here, I failed, but I waited whole 1 sec and finished thread");
+        });
+    }
+
+    @Disabled
+    @Test
+    void testTimeOutPrempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(8000);
+            System.out.println("I do not got here, I waited only 0,1 sec and I failed");
+        });
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNETIME")));
+    }
+
+    @Test
+    void testAssumptionTrueAssumptionIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
     }
 }
