9a04d2fb8d6d2e00f92da312921d9492d90ac1f3
kamildyla <kamil.dyla09@gmail.com>
2020-12-12 18:54:32 +0100

    repeated test

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -17,5 +19,11 @@ class PersonTest implements ModelTests {
         assertAll("Test Props Set",
                 () -> assertEquals("Joe", person.getFirstName(), "Wrong firstName"),
                 () -> assertEquals("Buck", person.getLastName(), "Wrong lastName"));
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} of {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    void myRepeatedTest() {
+        //todo impl
     }
 }
